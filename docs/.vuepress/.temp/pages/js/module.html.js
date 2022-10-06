a8b0ed434a07ea230d6ed573d8a6d35211aee6fe
SuzumiyaHaku <602966707@qq.com>
2022-10-07 00:30:12 +0800

    初始化

--- docs/.vuepress/.temp/pages/js/module.html.js
+++ docs/.vuepress/.temp/pages/js/module.html.js
@@ -1,14 +1,1 @@
 export const data = JSON.parse("{\"key\":\"v-02e54d60\",\"path\":\"/js/module.html\",\"title\":\"CommonJS、AMD、CMD、ESM、UMD\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"CommonJS\",\"slug\":\"commonjs\",\"link\":\"#commonjs\",\"children\":[{\"level\":3,\"title\":\"解析该模块node做什么\",\"slug\":\"解析该模块node做什么\",\"link\":\"#解析该模块node做什么\",\"children\":[]}]},{\"level\":2,\"title\":\"AMD\",\"slug\":\"amd\",\"link\":\"#amd\",\"children\":[]},{\"level\":2,\"title\":\"CMD\",\"slug\":\"cmd\",\"link\":\"#cmd\",\"children\":[{\"level\":3,\"title\":\"UMD\",\"slug\":\"umd\",\"link\":\"#umd\",\"children\":[]},{\"level\":3,\"title\":\"ESM\",\"slug\":\"esm\",\"link\":\"#esm\",\"children\":[]},{\"level\":3,\"title\":\"推荐阅读\",\"slug\":\"推荐阅读\",\"link\":\"#推荐阅读\",\"children\":[]}]}],\"git\":{},\"filePathRelative\":\"js/module.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
