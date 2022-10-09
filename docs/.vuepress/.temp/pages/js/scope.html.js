6f3809fcf0d3b084e93ff6586b1704b9855a32cc
SuzumiyaHaku <602966707@qq.com>
2022-10-09 21:52:02 +0800

    初始化

--- docs/.vuepress/.temp/pages/js/scope.html.js
+++ docs/.vuepress/.temp/pages/js/scope.html.js
@@ -1,14 +1,1 @@
 export const data = JSON.parse("{\"key\":\"v-5953e59c\",\"path\":\"/js/scope.html\",\"title\":\"作用域\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"参考\",\"slug\":\"参考\",\"link\":\"#参考\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"js/scope.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
